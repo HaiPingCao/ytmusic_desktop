b66480d32fadabe28d1ad9cc824da04db407f392
thedtvn <duongtuan30306@gmail.com>
2025-07-27 15:38:32 +0700

    feat: Add fullscreen change event listener and toggle fullscreen on F11 key press

--- src-tauri/inject-src/load_lib.js
+++ src-tauri/inject-src/load_lib.js
@@ -1,13 +1,19 @@
 import * as tauriAppsapi from 'https://cdn.jsdelivr.net/npm/@tauri-apps/api@1.6.0/index.min.js';
 import "https://cdn.jsdelivr.net/npm/dompurify@3.2.6/dist/purify.min.js";
+
 window.tauri_api = tauriAppsapi;
 window.addEventListener('load', () => {
     import("./inject.js");
     import("./plugin_inject.js");
-}, { once: true });
+}, {once: true});
 
-window.addEventListener('keydown', async (event) => {                                 
-    console.log("Key pressed:", event.key);
+document.addEventListener('fullscreenchange', async () => {
+    let window_tauri = await window.tauri_api.window.getCurrent();
+    window_tauri.setFullscreen(!Boolean(document.fullscreenElement));
+});
+
+
+window.addEventListener('keydown', async (event) => {
     if (event.key === 'F11') {
         let window_tauri = await window.tauri_api.window.getCurrent();
         let isFullscreen = await window_tauri.isFullscreen();
